e47d27d63ffe759aff17ae1d9ac8be7c55418086
Alex Parog <sbibik2000@mail.ru>
2025-01-23 11:31:18 +0300

    feat: enum для сущности Account

---
+++ src/main/java/ru/t1/java/demo/model/AccountTypeEnum.java
@@ -0,0 +1,63 @@
+package ru.t1.java.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+/**
+ * Enum, представляющий тип счета {@link Account}.
+ */
+@Getter
+public enum AccountTypeEnum {
+    /**
+     * Дебетовый счет.
+     */
+    DEBIT("DEBIT"),
+
+    /**
+     * Кредитный счет.
+     */
+    CREDIT("CREDIT");
+
+    /**
+     * Строковое значение счета.
+     */
+    @JsonValue
+    private final String value;
+
+    /**
+     * Конструктор для создания типа счета с указанным строковым значением.
+     *
+     * @param value строковое представление счета
+     */
+    AccountTypeEnum(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Преобразует строковое значение в соответствующий тип счета.
+     *
+     * @param value строковое представление счета
+     * @return объект {@link AccountTypeEnum}, соответствующий строковому значению,
+     * или {@code null}, если значение некорректно
+     */
+    @JsonCreator
+    public AccountTypeEnum fromString(String value) {
+        return Arrays.stream(values())
+                .filter(status -> status.value.toLowerCase().equals(value))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * Возвращает строковое представление счета.
+     *
+     * @return строковое значение счета
+     */
+    @Override
+    public String toString() {
+        return value;
+    }
+}
